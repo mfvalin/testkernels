a37254735f3e14c7ab19f709af1f8168a72b57b6
M. Valin <mfvalin@gmail.com>
2019-02-15 10:56:31 -0500

    added mpi init + finalize

--- Epi/test_kernel_matvec2.F90
+++ Epi/test_kernel_matvec2.F90
@@ -2,11 +2,14 @@ program test_kernel_matvec2
   use kmv2
   implicit none
   include 'mpif.h'
+  integer :: ier
   real *8 :: t0, t1
+  call mpi_init(ier)
   call initialize()
 ! call kernel_matvec2 (nu   , dt   , i0, in  , j0, jn  , ns)
   t0 = MPI_wtime()
   call kernel_matvec2 (1.0_8, 2.0_8, 4 , l_ni-3, 4 , l_nj-3, 3 )
   t1 = MPI_wtime()
   print *, 'time=',nint(t1-t0)*1000,' msecs'
+  call mpi_finalize(ier)
 end program test_kernel_matvec2
